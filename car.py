c374fe6b17856e4b3785c79888bf195114546e01
himanshi <hv.himanshi.verma@gmail.com>
2020-04-16 21:59:02 +0530

    score

--- car.py
+++ car.py
@@ -11,6 +11,8 @@ black = (0,0,0)
 white = (255,255,255)
 red = (255,0,0)
 
+block_color = (53,115,255)
+
 car_width = 73
 
 gameDisplay = pygame.display.set_mode((display_width,display_height))
@@ -18,6 +20,12 @@ pygame.display.set_caption('A bit Racey')
 clock = pygame.time.Clock()
 
 carImg = pygame.image.load('racecar.png')
+
+def things_dodged(count):
+    font = pygame.font.SysFont(None, 25)
+    text = font.render("Dodged: "+str(count), True, black)
+    gameDisplay.blit(text,(0,0))
+
 
 def things(thingx, thingy, thingw, thingh, color):
     pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
@@ -57,6 +65,8 @@ def game_loop():
     thing_width = 100
     thing_height = 100
 
+    thingCount = 1
+    dodged = 0
 
 
     gameExit = False
@@ -83,8 +93,10 @@ def game_loop():
         gameDisplay.fill(white)
 
         things(thing_startx, thing_starty, thing_width, thing_height, black)
+        
         thing_starty += thing_speed
         car(x,y)
+        things_dodged(dodged)
 
         if x > display_width - car_width or x < 0:
             crash()
@@ -92,6 +104,16 @@ def game_loop():
         if thing_starty > display_height:
             thing_starty = 0 - thing_height
             thing_startx = random.randrange(0,display_width)
+            dodged += 1
+            thing_speed += 1
+            thing_width += (dodged * 1.2)
+            
+        if y < thing_starty+thing_height:
+            print('y crossover')
+
+            if x > thing_startx and x < thing_startx + thing_width or x+car_width > thing_startx and x + car_width < thing_startx+thing_width:
+                print('x crossover')
+                crash()
 
         pygame.display.update()
         clock.tick(60)
